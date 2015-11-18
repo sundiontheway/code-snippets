daf7a57722fa2ba115090a47cd216c960f125cc9
Sun Di <sundi1@syswin.com>
2015-11-18 15:57:36 +0800

    Update progress

--- progress.py
+++ progress.py
@@ -3,7 +3,21 @@ from sys import stdout
 import time
 
 def progress(width, percent):
-    """ 终端进度条 """
+    """ ====       4/51 """
+
+    if percent > 100:
+        stdout.write('')
+        stdout.flush()
+        return
+
+    print_str = "%s %d/%d\r" % \
+        (('%%-%ds' % width) % (width * percent / 100 * '='), percent, width)
+
+    stdout.write(print_str)
+    stdout.flush()
+
+def progress_100(percent):
+    """ =====       5%  """
 
     if percent > 100:
         stdout.write('')
@@ -11,14 +25,16 @@ def progress(width, percent):
         return
 
     print_str = "%s %d%%\r" % \
-        (('%%-%ds' % width) % (width * percent / 100 * '='), percent)
+        (('%%-%ds' % 100) % (100 * percent / 100 * '='), percent)
 
     stdout.write(print_str)
     stdout.flush()
 
+
 if __name__ == "__main__":
     for i in range(1, 100):
-        progress(100, i)
+        # progress_100(i)
+        progress(120, i)
         time.sleep(1)
 
 
